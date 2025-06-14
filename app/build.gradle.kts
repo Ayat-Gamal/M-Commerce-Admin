c014bccfaeea09589629800f6248f4d4e594c4b8
Ayat Gamal <ayat.ga106@gmail.com>
2025-06-15 00:16:15 +0300

    add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,11 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+    id("com.google.devtools.ksp")
+     id("com.google.dagger.hilt.android")
+    //apollo
+    id("com.apollographql.apollo") version "4.3.0"
 }
 
 android {
@@ -40,6 +45,7 @@ android {
 }
 
 dependencies {
+    val room_version = "2.6.1"
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
@@ -56,4 +62,29 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+
+    //Coil
+    implementation("io.coil-kt.coil3:coil-compose:3.0.4")
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.0.4")
+
+    //room
+    implementation("androidx.room:room-runtime:$room_version")
+   ksp("androidx.room:room-compiler:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
+
+    //hilt
+    implementation("com.google.dagger:hilt-android:2.49")
+    ksp("com.google.dagger:hilt-android-compiler:2.49")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.5")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+
+    //apollo
+    implementation("com.apollographql.apollo:apollo-runtime:4.3.0")
+}
+
+apollo {
+    service("service") {
+        packageName.set("com.example.m_commerce_admin")
+    }
 }
