1be23b50256d13f514d9408a9f943a5044737b39
Ayat Gamal <ayat.ga106@gmail.com>
2025-06-18 03:30:24 +0300

    handle constans

--- app/src/main/java/com/example/m_commerce_admin/config/constant/NavRoutes.kt
+++ app/src/main/java/com/example/m_commerce_admin/config/constant/NavRoutes.kt
@@ -6,3 +6,12 @@ enum class NavRoutes {
     PRODUCT,
     INVENTORY
 }
+
+class Screens {
+    companion object {
+        const val HOME_SCREEN = "Home"
+        const val PRODUCT_SCREEN = "Product"
+        const val INVENTORY_SCREEN = "Inventory"
+        const val COUPONS_SCREEN = "Coupons"
+    }
+}
